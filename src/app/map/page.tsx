11c0fa7ac4a4ad1a22c9a09437e537ad8c729add
Luca Berres <luca.berres@gmx.de>
2025-12-02 19:15:27 +0100

    feat: add MapPolyline component to visualize city coordinates on the map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,28 +1,31 @@
-import { Map, MapMarker, MapTileLayer } from "@/components/ui/map"
-import type { LatLngExpression } from "leaflet"
+import { Map, MapMarker, MapTileLayer, MapPolyline } from "@/components/ui/map";
+import type { LatLngExpression } from "leaflet";
 
 export default function MapWithMarkers() {
-    const CITIES = [
-        {
-            name: "Toronto",
-            coordinates: [43.6532, -79.3832] satisfies LatLngExpression,
-        },
-        {
-            name: "Port Elizabeth",
-            coordinates: [-33.9137, 25.5827] satisfies LatLngExpression,
-        },
-        {
-            name: "Hong Kong",
-            coordinates: [22.3193, 114.1694] satisfies LatLngExpression,
-        },
-    ]
+  const CITIES = [
+    {
+      name: "Toronto",
+      coordinates: [43.6532, -79.3832] satisfies LatLngExpression,
+    },
+    {
+      name: "Port Elizabeth",
+      coordinates: [-33.9137, 25.5827] satisfies LatLngExpression,
+    },
+    {
+      name: "Hong Kong",
+      coordinates: [22.3193, 114.1694] satisfies LatLngExpression,
+    },
+  ];
 
-    return (
-        <Map center={CITIES[1].coordinates} zoom={1}>
-            <MapTileLayer />
-            {CITIES.map((city) => (
-                <MapMarker key={city.name} position={city.coordinates} />
-            ))}
-        </Map>
-    )
+  const POINTS: LatLngExpression[] = CITIES.map((city) => city.coordinates);
+
+  return (
+    <Map center={CITIES[1].coordinates} zoom={1}>
+      <MapTileLayer />
+      {CITIES.map((city) => (
+        <MapMarker key={city.name} position={city.coordinates} />
+      ))}
+      <MapPolyline positions={POINTS} fillColor="white" />
+    </Map>
+  );
 }
