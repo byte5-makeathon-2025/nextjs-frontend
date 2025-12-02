084191eaed119f2b293396dd40e1be5cf399b163
Luca Berres <luca.berres@gmx.de>
2025-12-02 20:26:07 +0100

    refactor: update city markers in the map component with new data

--- src/app/dashboard/map/page.tsx
+++ src/app/dashboard/map/page.tsx
@@ -5,31 +5,66 @@ import {
   MapPolyline,
   MapPopup,
 } from "@/components/ui/map";
-import { icon, type LatLngExpression } from "leaflet";
+import { type LatLngExpression } from "leaflet";
 
 export default function MapWithMarkers() {
   const CITIES = [
     {
-      name: "Toronto",
-      coordinates: [43.6532, -79.3832] satisfies LatLngExpression,
+      name: "Berlin",
+      coordinates: [52.52, 13.405] satisfies LatLngExpression,
+      number: 0,
+    },
+    {
+      name: "Paris",
+      coordinates: [48.8566, 2.3522] satisfies LatLngExpression,
       number: 1,
     },
     {
-      name: "Port Elizabeth",
-      coordinates: [-33.9137, 25.5827] satisfies LatLngExpression,
+      name: "Frankfurt",
+      coordinates: [50.1109, 8.6821] satisfies LatLngExpression,
       number: 2,
     },
     {
-      name: "Hong Kong",
-      coordinates: [22.3193, 114.1694] satisfies LatLngExpression,
+      name: "Rom",
+      coordinates: [41.9028, 12.4964] satisfies LatLngExpression,
       number: 3,
+    },
+    {
+      name: "Madrid",
+      coordinates: [40.4168, -3.7038] satisfies LatLngExpression,
+      number: 4,
+    },
+    {
+      name: "London",
+      coordinates: [51.5074, -0.1278] satisfies LatLngExpression,
+      number: 5,
+    },
+    {
+      name: "Mailand",
+      coordinates: [45.4642, 9.19] satisfies LatLngExpression,
+      number: 6,
+    },
+    {
+      name: "Stockholm",
+      coordinates: [59.3293, 18.0686] satisfies LatLngExpression,
+      number: 7,
+    },
+    {
+      name: "San Francisco",
+      coordinates: [37.7749, -122.4194] satisfies LatLngExpression,
+      number: 8,
+    },
+    {
+      name: "Los Angeles",
+      coordinates: [34.0522, -118.2437] satisfies LatLngExpression,
+      number: 9,
     },
   ];
 
   const POINTS: LatLngExpression[] = CITIES.map((city) => city.coordinates);
 
   return (
-    <Map center={CITIES[1].coordinates} zoom={1}>
+    <Map center={CITIES[0].coordinates} zoom={3}>
       <MapTileLayer />
       {CITIES.map((city) => (
         <MapMarker
