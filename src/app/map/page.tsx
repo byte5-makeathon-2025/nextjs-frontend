85a037c6c817a5e03021e9116a68af97f41d9d61
Luca Berres <luca.berres@gmx.de>
2025-12-02 19:42:42 +0100

    feat: add icons to city markers in the map component

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,19 +1,22 @@
 import { Map, MapMarker, MapTileLayer, MapPolyline } from "@/components/ui/map";
-import type { LatLngExpression } from "leaflet";
+import { icon, type LatLngExpression } from "leaflet";
 
 export default function MapWithMarkers() {
   const CITIES = [
     {
       name: "Toronto",
       coordinates: [43.6532, -79.3832] satisfies LatLngExpression,
+      icon: <span className="text-sm">🎅1</span>,
     },
     {
       name: "Port Elizabeth",
       coordinates: [-33.9137, 25.5827] satisfies LatLngExpression,
+      icon: <span className="text-sm">🎅2</span>,
     },
     {
       name: "Hong Kong",
       coordinates: [22.3193, 114.1694] satisfies LatLngExpression,
+      icon: <span className="text-sm">🎅3</span>,
     },
   ];
 
@@ -23,7 +26,11 @@ export default function MapWithMarkers() {
     <Map center={CITIES[1].coordinates} zoom={1}>
       <MapTileLayer />
       {CITIES.map((city) => (
-        <MapMarker key={city.name} position={city.coordinates} />
+        <MapMarker
+          key={city.name}
+          position={city.coordinates}
+          icon={city.icon}
+        />
       ))}
       <MapPolyline positions={POINTS} fillColor="white" />
     </Map>
